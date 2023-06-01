b57b717a09989bb41f762bb36e93268c75c0ef75
urena-code <luisurenaa@gmail.com>
2023-06-01 23:20:27 +0000

    remove API key due to policy issues

--- codeToTestOpenAIAPI/openAI_communications_test.py
+++ codeToTestOpenAIAPI/openAI_communications_test.py
@@ -9,7 +9,7 @@ import os
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key =  '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'
+openai.api_key = 'key_here'
 
 response = openai.Completion.create(model="text-davinci-003", prompt="hello chatgpt can you hear meee", temperature=0, max_tokens=7)
 print(response["choices"][0]["text"])
